f56289985502ee7b2bb0939933db9c188c20d491
Sergey Zhemchuzhny <sergey.zhemchuzhny@gmail.com>
2022-03-14 20:12:14 -0400

    added user detail components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { UserListComponent } from './user-list/user-list.component';
+import { AboutComponent } from './features/about/about.component';
+import { UserListComponent } from './features/user-list/user-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UserDetailComponent } from './features/user-detail/user-detail.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, AboutComponent, UserListComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgbModule],
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    UserListComponent,
+    UserDetailComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
