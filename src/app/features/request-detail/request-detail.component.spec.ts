31ea571f23a9f13d3029eb0b45f6f12e5689ade4
Sergey Zhemchuzhny <sergey.zhemchuzhny@gmail.com>
2022-03-19 13:07:55 -0400

    created user profile

--- src/app/features/request-detail/request-detail.component.spec.ts
+++ src/app/features/request-detail/request-detail.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AboutComponent } from './about.component';
+import { RequestDetailComponent } from './request-detail.component';
 
-describe('AboutComponent', () => {
-  let component: AboutComponent;
-  let fixture: ComponentFixture<AboutComponent>;
+describe('RequestDetailComponent', () => {
+  let component: RequestDetailComponent;
+  let fixture: ComponentFixture<RequestDetailComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AboutComponent ]
+      declarations: [ RequestDetailComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AboutComponent);
+    fixture = TestBed.createComponent(RequestDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/models/request.model.ts
@@ -0,0 +1,11 @@
+export class Request {
+  User: user;
+	description: string;
+	justification: string;
+	LocalDate:  date;
+	deliveryMode: string;
+	status: string;
+	total: number;
+	submittedDate date;
+	reasonForRejection string;
+}
